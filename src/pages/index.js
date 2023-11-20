afef013b545ba8d23bc20a23707d7e1794a892a7
Aminah Makhoul <aminahmakhoul@gmail.com>
2023-11-19 23:14:09 -0300

    Fazendo o cabecalho

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Button from '@/components/Button'
+import Cabecalho from '@/components/Cabecalho'
+
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Séries</title>
+      </Head>
+
+      <Cabecalho/>
+      
+     
+    </>
+  )
+}
